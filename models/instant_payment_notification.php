6038193af65b3feaa4e715d1a7abc5dd483c92f0
webtechnick <nick@webtechnick.com>
2009-07-23 07:58:00 +0000

    Some minor code cleanup.

--- models/instant_payment_notification.php
+++ models/instant_payment_notification.php
@@ -35,22 +35,22 @@ class InstantPaymentNotification extends PaypalIpnAppModel {
         }
         
         //and post the transaction back for validation
-        $fp = fsockopen ("ssl://$server", 443, $errno, $errstr, 30);
-        if (!$fp) {
+        $fp = fsockopen("ssl://$server", 443, $errno, $errstr, 30);
+        if(!$fp) {
           //...didn't get a response so log error in error logs
-          $this->log('HTTP Error in InstantPaymentNotifications::process while posting back to PayPal: Transaction='.$transaction);
+          $this->log('HTTP Error in InstantPaymentNotification::process while posting back to PayPal: Transaction='.$transaction);
         }
         else {
-          //...got a response, so we'll through the response looking for VERIFIED or INVALID
+          //...got a response, so we'll go through the response looking for VERIFIED or INVALID
           fputs($fp, $header . $transaction);
           while (!feof($fp)) {
-            $response = fgets ($fp, 1024);
-            if (strcmp($response, "VERIFIED") == 0) {
-              return true; //Its verified.
-            }
-            elseif(strcmp ($response, "INVALID") == 0) {
+            $response = fgets($fp, 1024);
+            if(strcmp($response, "INVALID") == 0) {
               //The response is INVALID so log it for investigation
               $this->log("Found Invalid: $transaction",'paypal');
+            }
+            elseif(strcmp($response, "VERIFIED") == 0) {
+              return true; //Its verified.
             }
           }
         }

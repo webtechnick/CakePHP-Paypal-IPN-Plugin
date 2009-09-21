91ec074694ad7ae90b6916dff977a2b52fa50875
webtechnick <nick@webtechnick.com>
2009-09-21 23:15:02 +0000

    reverted configuration changes.

--- config/paypal_ipn_config.php
+++ config/paypal_ipn_config.php
@@ -5,26 +5,15 @@
   * 
   * All these options can be set on the fly as well within the helper
   */
-  
-  define('LIVE_PAYPAL', Configure::read('PB.paypal'));
-  define('TEST_PAYPAL', Configure::read('PB.test_paypal'));
-  define('NOTIFY_URL', Configure::read('PB.paypal_ipn'));
-
-/************
-  * Use these settings to set defaults for the Paypal Helper class.
-  * The PaypalHelper class will help you create paynow, subscribe, donate, or addtocart buttons for you.
-  * 
-  * All these options can be set on the fly as well within the helper
-  */
 class PaypalIpnConfig {
 
   /************
     * Each settings key coresponds to the Paypal API.  Review www.paypal.com for more. 
     */
   var $settings = array(
-    'business' => LIVE_PAYPAL, //Your Paypal email account
+    'business' => 'live_email@paypal.com', //Your Paypal email account
     'server' => 'https://www.paypal.com', //Main paypal server.
-    'notify_url' => NOTIFY_URL, //Notify_url... set this to the process path of your paypal_ipn::instant_payment_notification::process action
+    'notify_url' => 'http://www.yoursite.com/paypal_ipn/process', //Notify_url... set this to the process path of your paypal_ipn::instant_payment_notification::process action
     'currency_code' => 'USD', //Currency
     'lc' => 'US', //Locality
     'item_name' => 'Paypal_IPN', //Default item name.
@@ -35,9 +24,9 @@ class PaypalIpnConfig {
     * Test settings to test with using a sandbox paypal account.
     */
   var $testSettings = array(
-    'business' => TEST_PAYPAL,
+    'business' => 'sandbox_email@paypal.com',
     'server' => 'https://www.sandbox.paypal.com',
-    'notify_url' => NOTIFY_URL,
+    'notify_url' => 'http://www.yoursite.com/paypal_ipn/process',
     'currency_code' => 'USD',
     'lc' => 'US',
     'item_name' => 'Paypal_IPN',

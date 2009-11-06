daf82394bdbaee3971d0080ebe2a334654ffe062
webtechnick <nick@webtechnick.com>
2009-11-06 06:11:46 +0000

    Updated the schema files for the new paypal_items table.

--- config/sql/ipn.php
+++ config/sql/ipn.php
@@ -100,6 +100,21 @@ class ipnSchema extends CakeSchema {
 		'modified' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
 		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1))
 	);
+	
+	var $paypal_items = array(
+		'id' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 36, 'key' => 'primary'),
+		'instant_payment_notification_id' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 36),
+		'item_name' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 127),
+		'item_number' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 127),
+		'quantity' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 127),
+		'mc_gross' => array('type' => 'float', 'null' => true, 'default' => NULL, 'length' => '10,2'),
+		'mc_shipping' => array('type' => 'float', 'null' => true, 'default' => NULL, 'length' => '10,2'),
+		'mc_handling' => array('type' => 'float', 'null' => true, 'default' => NULL, 'length' => '10,2'),
+		'tax' => array('type' => 'float', 'null' => true, 'default' => NULL, 'length' => '10,2'),
+		'created' => array('type' => 'datetime', 'null' => false, 'default' => NULL),
+		'modified' => array('type' => 'datetime', 'null' => false, 'default' => NULL),
+		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1))
+	);
 
 }
 ?>
